a7c2bce2aa7a04f49dc906085c879d5969d411a0
Đậu Khoa <khoakomlem@gmail.com>
2024-06-28 20:53:59 +0700

    setup project

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	i18n: {
-		locales: ["en", "vi"],
-		defaultLocale: "en",
-	},
-	env: {
-		API_URL: process.env.API_URL,
-		API_VERSION: process.env.API_VERSION,
-	},
-}
+  reactStrictMode: false,
+  i18n: {
+    locales: ["en", "vi"],
+    defaultLocale: "en",
+  },
+  env: {
+    API_URL: process.env.API_URL,
+    API_VERSION: process.env.API_VERSION,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,9 @@ const config = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        momo: {
+          DEFAULT: "rgb(193 23 124)",
+        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
