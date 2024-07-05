bdf0ff13b2aba6c3a05acec0f1c7c2b6fef5d240
Đậu Khoa <khoakomlem@gmail.com>
2024-07-06 03:13:58 +0700

    react type

--- src/components/icon/PlayIcon.tsx
+++ src/components/icon/PlayIcon.tsx
@@ -12,11 +12,11 @@ export function PlayIcon({
       xmlns='http://www.w3.org/2000/svg'
       onClick={onClick}
     >
-      <g fill='none' fill-rule='evenodd'>
+      <g fill='none' fillRule='evenodd'>
         <path
           d='M34.667 24.335c0 .515-.529.885-.529.885l-14.84 9.133c-1.08.704-1.965.182-1.965-1.153V15.467c0-1.338.884-1.856 1.968-1.153L34.14 23.45c-.002 0 .527.37.527.885Z'
           fill='currentColor'
-          fill-rule='nonzero'
+          fillRule='nonzero'
         />
       </g>
     </svg>
