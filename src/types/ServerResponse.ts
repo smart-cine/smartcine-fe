1006c33d1354f2290b0bd88056d29f5019a5589e
Đậu Khoa <khoakomlem@gmail.com>
2024-07-06 03:15:33 +0700

    fix name response type

--- src/types/ServerResponse.ts
+++ src/types/ServerResponse.ts
@@ -1,4 +1,12 @@
-export type ServerResponse<Data = any> = {
-  success: boolean;
+export type SuccessRes<Data = any> = {
+  success: true;
   data: Data;
+};
+
+export type SuccessPaginationRes<Item> = SuccessRes<Item[]> & {
+  pagination: {
+    total: number;
+    page: number;
+    limit: number;
+  };
 };
