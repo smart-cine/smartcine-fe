58704fcc7181c0f546266406ed7828173fb90a60
Đậu Khoa <khoakomlem@gmail.com>
2024-07-13 01:46:59 +0700

    useScreenDetector

---
+++ src/hooks/useScreenDetector.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export const useScreenDetector = () => {
+  const [width, setWidth] = useState(0);
+
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    window.addEventListener('resize', handleWindowSizeChange);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+  const isTablet = width <= 1024;
+  const isDesktop = width > 1024;
+
+  return { isMobile, isTablet, isDesktop };
+};
