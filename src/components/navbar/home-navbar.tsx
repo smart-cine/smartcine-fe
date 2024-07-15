930afbd19b8e6a7c1b06ef0235682036485c5352
Đậu Khoa <khoakomlem@gmail.com>
2024-07-15 11:53:12 +0700

    update navbar lib

--- src/components/navbar/home-navbar.tsx
+++ src/components/navbar/home-navbar.tsx
@@ -1,11 +1,6 @@
-import { useEffect, useRef, useState } from 'react';
-import Image from 'next/image';
+import { forwardRef, useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
-import {
-  NavigationMenuIndicator,
-  NavigationMenuViewport,
-} from '@radix-ui/react-navigation-menu';
-import { DotIcon, MenuIcon } from 'lucide-react';
+import { MenuIcon } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 import {
@@ -18,47 +13,52 @@ import {
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
 
-export function Navbar({
-  className,
-  children,
-}: {
-  readonly className?: string;
-  readonly children?: React.ReactNode;
-}) {
-  return (
-    <div
-      className={cn(
-        'mx-auto flex h-[56px] flex-row items-center border-b-[2px] bg-white px-4 py-2 shadow-sm lg:px-24',
-        className
-      )}
-    >
-      <div className='flex w-full flex-row'>
-        <Link href='/' className='flex items-center'>
-          <div className='flex flex-row items-center gap-x-2.5'>
-            <Image
-              src='/smartcine-logo.svg'
-              alt='smartcine logo'
-              width={35}
-              height={35}
-            />
-            <p className='font-bold leading-6 text-[#825AD8]'>SmartCine</p>
-          </div>
-        </Link>
-        <div className='grow' />
-        <div className='hidden flex-row lg:flex'>
-          <NavigationMenu>
-            <NavigationMenuList>{children}</NavigationMenuList>
-          </NavigationMenu>
-        </div>
-      </div>
-      <div className='flex w-full flex-row justify-end lg:hidden'>
-        <MenuIcon className='h-6 w-6' />
-      </div>
-    </div>
-  );
-}
+import { CameraIcon } from '../icon/CameraIcon';
+import { Logo } from '../icon/Logo';
+import { ModeToggle } from '../other/ModeToggle';
+import { Separator } from '../ui/separator';
 
-Navbar.Link = NavBarLink;
+export const Navbar = forwardRef<
+  HTMLUListElement,
+  {
+    readonly className?: string;
+    readonly children?: React.ReactNode;
+  }
+>(({ className, children }, ref) => (
+  <div
+    className={cn(
+      'mx-auto flex h-[65px] flex-row items-center border-b-[2px] bg-white px-4 py-2 shadow-sm lg:px-24',
+      className
+    )}
+  >
+    <div className='flex w-full flex-row'>
+      <Link href='/' className='flex items-center'>
+        <div className='flex flex-row items-center gap-x-2.5'>
+          <Logo className='h-11 w-11' />
+          <Separator orientation='vertical' className='min-h-7' />
+          <CameraIcon className='h-9 w-9' />
+          <p className='text-sm font-semibold leading-4 text-momo'>
+            Đặt vé
+            <br />
+            xem phim
+          </p>
+        </div>
+      </Link>
+      <div className='grow' />
+      <div className='hidden flex-row lg:flex'>
+        <NavigationMenu>
+          <NavigationMenuList ref={ref}>{children}</NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <ModeToggle className='ml-1' />
+    </div>
+    <div className='flex w-full flex-row justify-end lg:hidden'>
+      <MenuIcon className='h-6 w-6' />
+    </div>
+  </div>
+));
+Navbar.displayName = 'Navbar';
+
 export function NavBarLink({
   children,
   href,
@@ -68,14 +68,19 @@ export function NavBarLink({
 }) {
   return (
     <NavigationMenuItem>
-      <NavigationMenuLink href={href} className={navigationMenuTriggerStyle()}>
+      <NavigationMenuLink
+        href={href}
+        className={cn(
+          navigationMenuTriggerStyle(),
+          'text-base font-bold text-gray-600'
+        )}
+      >
         {children}
       </NavigationMenuLink>
     </NavigationMenuItem>
   );
 }
 
-Navbar.Item = NavbarItem;
 export function NavbarItem({
   children,
   title,
@@ -106,12 +111,17 @@ export function NavbarItem({
 
   return (
     <NavigationMenuItem>
-      <NavigationMenuTrigger ref={ref}>{title}</NavigationMenuTrigger>
+      <NavigationMenuTrigger
+        ref={ref}
+        className='text-base font-bold text-gray-600'
+      >
+        {title}
+      </NavigationMenuTrigger>
       <NavigationMenuContent
         style={{
           width,
         }}
-        className={cn('p-4', className)}
+        className={cn('p-4 text-base font-bold text-gray-600', className)}
       >
         {children}
       </NavigationMenuContent>
