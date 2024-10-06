99af9ffb77f46bca7c250a4c92ad9cb6512f8e3c
Đậu Khoa <khoakomlem@gmail.com>
2024-10-06 16:20:36 +0700

    fix perform type to new api

--- src/core/perform/perform.query.ts
+++ src/core/perform/perform.query.ts
@@ -4,8 +4,17 @@ import { type TPerform } from './perform.type';
 
 export const {
   create: useCreatePerform,
-  list: useListPerform,
   read: useReadPerform,
   patch: usePatchPerform,
-  delete: useDeletePerform,
+  delete: useDeletePerformBy,
 } = genQueryCrud<TPerform>('perform');
+
+export const useListPerformByCinema = genQueryCrud<{
+  cinema_id: string;
+  performs: TPerform[];
+}>('perform/list-cinema').list;
+
+export const useListPerformByFilm = genQueryCrud<{
+  film_id: string;
+  performs: TPerform[];
+}>('perform/list-film').list;
