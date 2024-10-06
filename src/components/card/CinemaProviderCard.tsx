593ee6acc158340cbd711e2a51445e0122baf8fb
Đậu Khoa <khoakomlem@gmail.com>
2024-10-06 16:19:06 +0700

    round score

--- src/components/card/CinemaProviderCard.tsx
+++ src/components/card/CinemaProviderCard.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { TagIcon } from 'lucide-react';
 
-import { cn } from '@/lib/utils';
+import { cn, roundScore } from '@/lib/utils';
 import { useReadCinemaProvider } from '@/core/cinema-provider/cinema-provider';
 
 import { MapPointIcon } from '../icon/MapPointIcon';
@@ -30,6 +30,7 @@ export function CinemaProviderCard({
         <div className='flex h-[60px] w-[100px] items-center justify-center'>
           <Image
             src={
+              // TODO: đổi thành constant unknown logo
               cinema_provider.logo_url ?? '/cinema-provider/logo/unknown.png'
             }
             alt='cinema-logo'
@@ -41,7 +42,7 @@ export function CinemaProviderCard({
           <p className='text-lg font-bold text-momo'>{cinema_provider.name}</p>
           <div className='text-sm'>
             <p className='text text-gray-500'>
-              Hệ thống rạp chiếu phim từ Hàn Quốc
+              Hệ thống rạp chiếu phim từ {cinema_provider.country}
             </p>
             <p className='flex flex-row items-center gap-x-1 text-momo'>
               <TagIcon className='h-4 w-4' />
@@ -50,9 +51,9 @@ export function CinemaProviderCard({
           </div>
           <div className='flex flex-row gap-x-1'>
             <span className='font-semibold'>
-              {cinema_provider.rating.score}
+              {roundScore(cinema_provider.rating.score)}
             </span>
-            <span className=''>⭐️</span>
+            ⭐️
           </div>
           <div className='flex flex-row items-center gap-x-1 text-sm text-gray-400'>
             <MapPointIcon className='h-4 w-4' />
