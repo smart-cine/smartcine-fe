f47a1417a1e4f44590ee6776d26985a5a21e55a1
Đậu Khoa <khoakomlem@gmail.com>
2024-10-06 16:19:42 +0700

    fix agetag css

--- src/components/AgeTag.tsx
+++ src/components/AgeTag.tsx
@@ -10,7 +10,7 @@ export function AgeTag({
   return (
     <div
       className={cn(
-        'max-h-[20px] max-w-fit rounded-sm bg-blue-500 p-0.5 px-1 text-xs font-semibold text-gray-100 text-opacity-95',
+        'flex max-h-[20px] max-w-fit items-center justify-center rounded-sm bg-blue-500 p-0.5 px-1 text-xs font-semibold text-gray-100 text-opacity-95',
         {
           'bg-blue-500': restrictAge >= 10,
           'bg-green-500': restrictAge >= 13,
@@ -20,7 +20,8 @@ export function AgeTag({
         className
       )}
     >
-      {restrictAge}+
+      <p>{restrictAge}</p>
+      <p className='relative -top-[1px]'>+</p>
     </div>
   );
 }
